22c7eb30147d050a1780feb57a721c9d32190061
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-25 15:06:04 -0400

    commit tree 32c110fc7e7a3790288721ecba37bc2e3108296b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(arr, string){
+  let value = arr.filter(function(str) {
+    return (str.toLowerCase() === string.toLowerCase())
+  })
+  if (value){
+    return value
+  }
+}
+
+function fuzzyMatch(arr, string){
+  let value = arr.filter(function(str){
+    return str.slice(0,2).toLowerCase() === string.slice(0,2).toLowerCase()
+  })
+  if (value){
+    return value
+  }
+
+}
+
+function matchName(arr, string){
+  let value = arr.filter(function(obj){
+    return obj.name.toLowerCase() === string.toLowerCase()
+  })
+  if (value)
+  return value
+}
+
+
